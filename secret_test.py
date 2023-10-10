780390509c72e2fc676512c8706602b638cb1de1
arocasan <mr@michelantonio.com>
2023-10-11 00:37:26 +0200

    testing sec

--- secret_test.py
+++ secret_test.py
@@ -23,3 +23,5 @@ print(best_key)
 key = "?sv=2022-11-02&ss=bfqt&srt=c&sp=rwdlacupiytfx&se=2023-10-11T06:24:53Z&st=2023-10-10T22:24:53Z&spr=https&sig=X%2BKypEjLTi081FeXzJqGwwbixBZEuro1e4Zi1WxKuX8%3D"
 
 new_key = "+GJw+6o3TMI35INO4JzVB9NpJLJnrxjCvLbSJAzbLdcpNHhqp8gCNPsXI0jp4QfeHL2nOO8KOoZQ+AStF0NkEQ=="
+
+nice = "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"
